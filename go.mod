64cf070e7a1b5c7acc0905b6776282443ae2681a
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-05-31 12:27:54 +0000

    Add layer normalization layer

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/itsubaki/gpt
 
 go 1.26.1
 
-require github.com/itsubaki/autograd v0.0.7-0.20260531115951-d2a9691ce8ad
+require github.com/itsubaki/autograd v0.0.7-0.20260531122623-2c7d487c01a6
--- layer/layer_norm.go
+++ layer/layer_norm.go
@@ -1,13 +1,26 @@
 package layer
 
 import (
+	F "github.com/itsubaki/autograd/function"
 	L "github.com/itsubaki/autograd/layer"
 	"github.com/itsubaki/autograd/variable"
 )
 
 var _ L.Layer = (*LayerNormT)(nil)
 
+func LayerNorm(embeddim int) *LayerNormT {
+	p := make(L.Parameters)
+	p.Add("gamma", variable.Ones(embeddim))
+	p.Add("beta", variable.Zeros(embeddim))
+
+	return &LayerNormT{
+		eps:        1e-5,
+		Parameters: p,
+	}
+}
+
 type LayerNormT struct {
+	eps float64
 	L.Parameters
 }
 
@@ -16,5 +29,12 @@ func (l *LayerNormT) First(x ...*variable.Variable) *variable.Variable {
 }
 
 func (l *LayerNormT) Forward(x ...*variable.Variable) []*variable.Variable {
-	return x
+	mean := F.Mean(-1)(x[0])
+	variance := F.Variance(-1)(x[0], mean)
+	normx := F.Div(F.Sub(x[0], mean), F.Pow(0.5)(F.AddC(l.eps, variance)))
+	y := F.Add(F.Mul(l.Parameters["gamma"], normx), l.Parameters["beta"])
+
+	return []*variable.Variable{
+		y,
+	}
 }
