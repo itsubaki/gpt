32b31574c8e0d8dceea7316ce4a806ccc1667943
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-06-02 22:11:38 +0900

    Fix stride

--- layer/rope.go
+++ layer/rope.go
@@ -45,36 +45,34 @@ func (l *RoPET) First(x ...*variable.Variable) *variable.Variable {
 }
 
 func (l *RoPET) Forward(x ...*variable.Variable) []*variable.Variable {
-	shape := x[0].Shape()
+	shape, stride := x[0].Shape(), x[0].Stride()
 	B, H, T, D := shape[0], shape[1], shape[2], shape[3]
+	sB, sH, sT, sD := stride[0], stride[1], stride[2], stride[3]
+
 	if D%2 != 0 {
 		panic("keydim must be even")
 	}
-
-	strideB := H * T * D
-	strideH := T * D
-	strideT := D
 	half := D / 2
 
 	cos := l.cos.Data
 	sin := l.sin.Data
 
 	for b := range B {
-		baseB := b * strideB
+		baseB := b * sB
 		for h := range H {
-			baseH := baseB + h*strideH
+			baseH := baseB + h*sH
 			for t := range T {
-				baseT := baseH + t*strideT
+				baseT := baseH + t*sT
 				angle := t * half
 				for i := range half {
-					evenIdx := baseT + 2*i
-					oddIdx := baseT + 2*i + 1
+					c := cos[angle+i]
+					s := sin[angle+i]
+
+					evenIdx := baseT + (2*i)*sD
+					oddIdx := baseT + (2*i+1)*sD
 
 					even := x[0].Data.Data[evenIdx]
 					odd := x[0].Data.Data[oddIdx]
-
-					c := cos[angle+i]
-					s := sin[angle+i]
 
 					x[0].Data.Data[evenIdx] = even*c - odd*s
 					x[0].Data.Data[oddIdx] = even*s + odd*c
