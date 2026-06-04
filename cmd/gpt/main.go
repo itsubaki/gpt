279ab365f1fd5e0adc384fdaf15e74de635b208c
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-06-05 05:40:05 +0900

    Update gpt

--- cmd/gpt/main.go
+++ cmd/gpt/main.go
@@ -5,24 +5,16 @@ import (
 	"math/rand"
 
 	F "github.com/itsubaki/autograd/function"
+	"github.com/itsubaki/autograd/hook"
 	O "github.com/itsubaki/autograd/optimizer"
 	"github.com/itsubaki/autograd/variable"
 	"github.com/itsubaki/gpt/model"
 )
 
-func sample(vocabSize, maxContextLen int) *variable.Variable {
-	tokens := make([]float64, maxContextLen)
-	for i := range tokens {
-		tokens[i] = float64(rand.Intn(vocabSize))
-	}
-
-	return variable.New(tokens...).Reshape(1, maxContextLen)
-}
-
-func batchY(B, C, vocabSize int) *variable.Variable {
+func batch(B, C, V int) *variable.Variable {
 	tokens := make([]float64, B*C)
 	for i := range tokens {
-		tokens[i] = float64(rand.Intn(vocabSize))
+		tokens[i] = float64(rand.Intn(V))
 	}
 
 	return variable.New(tokens...).Reshape(B, C)
@@ -36,6 +28,7 @@ func main() {
 	numOfBlocks := 6
 	ffdim := 4 * embeddim
 	theta := 10000.0
+	batchSize := 1
 
 	m := model.NewGPT(
 		vocabSize,
@@ -47,17 +40,28 @@ func main() {
 		theta,
 	)
 
-	o := O.SGD{
-		LearningRate: 0.01,
+	o := O.AdamW{
+		Adam: O.Adam{
+			Alpha: 0.001,
+			Beta1: 0.9,
+			Beta2: 0.999,
+			Hook: []O.Hook{
+				hook.ClipGrad(1.0),
+			},
+		},
+		WeightDecay: 0.01,
 	}
 
 	// batch
-	x := sample(vocabSize, maxContextLen)
-	y := F.Reshape(-1)(batchY(1, maxContextLen, vocabSize)) // (B, C) -> (B*C)
+	x := batch(batchSize, maxContextLen, vocabSize)
+	y := batch(batchSize, maxContextLen, vocabSize)
 
 	// forward
 	logits := m.Forward(x)
-	loss := F.CrossEntropy(F.Reshape(-1, vocabSize)(logits), y) // (B, C, V) -> (B*C, V)
+	loss := F.CrossEntropy(
+		F.Reshape(-1, vocabSize)(logits), // (B, C, V) -> (B*C, V)
+		F.Reshape(-1)(y),                 // (B, C) -> (B*C)
+	)
 	fmt.Println(logits.Shape())
 	fmt.Println(loss.At())
 
