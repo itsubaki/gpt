d911961c6a3fffd116e9b963ceaeddf824aab797
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-06-04 23:27:39 +0900

    Update gpt

--- cmd/gpt/main.go
+++ cmd/gpt/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"math/rand"
 
+	F "github.com/itsubaki/autograd/function"
+	O "github.com/itsubaki/autograd/optimizer"
 	"github.com/itsubaki/autograd/variable"
 	"github.com/itsubaki/gpt/model"
 )
@@ -15,6 +17,15 @@ func sample(vocabSize, maxContextLen int) *variable.Variable {
 	}
 
 	return variable.New(tokens...).Reshape(1, maxContextLen)
+}
+
+func batchY(B, C, vocabSize int) *variable.Variable {
+	tokens := make([]float64, B*C)
+	for i := range tokens {
+		tokens[i] = float64(rand.Intn(vocabSize))
+	}
+
+	return variable.New(tokens...).Reshape(B, C)
 }
 
 func main() {
@@ -36,15 +47,28 @@ func main() {
 		theta,
 	)
 
+	o := O.SGD{
+		LearningRate: 0.01,
+	}
+
+	// forward
 	x := sample(vocabSize, maxContextLen)
 	logits := m.Forward(x)
-	fmt.Println(logits.Shape()) // [1 256 1000]
+	fmt.Println(logits.Shape()) // (B, C, V) = [1 256 1000]
 
-	logits.Backward()
+	// loss
+	y := F.Reshape(-1)(batchY(1, maxContextLen, vocabSize))     // (B, C) -> (B*C)
+	loss := F.CrossEntropy(F.Reshape(-1, vocabSize)(logits), y) // (B, C, V) -> (B*C, V)
 
+	// backward and update
+	m.Cleargrads()
+	loss.Backward()
+	o.Update(m)
+
+	// print param shapes and total param count
 	var total int
-	for name, param := range m.Params().Seq2() {
-		fmt.Println(name, param.Shape(), param.Grad.Shape())
+	for _, param := range m.Params().Seq2() {
+		_, _ = param.Shape(), param.Grad.Shape()
 		total += param.Size()
 	}
 
