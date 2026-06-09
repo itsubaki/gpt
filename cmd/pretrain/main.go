0885701f1478968b236859ada964d4a66ba59579
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-06-09 22:41:04 +0900

    Update context-len

--- cmd/pretrain/main.go
+++ cmd/pretrain/main.go
@@ -26,7 +26,7 @@ func main() {
 	flag.IntVar(&maxIters, "max-iters", 40000, "number of maximum iterations")
 	flag.IntVar(&batchSize, "batch-size", 32, "batch size")
 	flag.IntVar(&vocabSize, "vocab-size", 1000, "vocabulary size")
-	flag.IntVar(&contextLen, "context-len", 256, "maximum context length")
+	flag.IntVar(&contextLen, "context-len", 128, "maximum context length")
 	flag.IntVar(&embeddim, "embeddim", 384, "embedding dimension")
 	flag.IntVar(&numOfHeads, "num-of-heads", 6, "number of heads")
 	flag.IntVar(&numOfBlocks, "num-of-blocks", 6, "number of blocks")
