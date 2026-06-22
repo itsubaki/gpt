45464b00ce4660031f80e3ee4e9c632aae0a6982
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-06-22 19:42:09 +0900

    Add loss

---
+++ grpo/loss.go
@@ -0,0 +1,32 @@
+package grpo
+
+import (
+	F "github.com/itsubaki/autograd/function"
+	"github.com/itsubaki/autograd/variable"
+)
+
+type Model interface {
+	Forward(x *variable.Variable) *variable.Variable
+}
+
+func ComputeProbs(model Model, ids *variable.Variable) *variable.Variable {
+	logits := model.Forward(ids)                      // (B, C, V)
+	logits = slice(logits, 1, 0, logits.Shape()[1]-1) // (B, C-1, V)
+	props := F.Softmax(-1)(logits)                    // (B, C-1, V)
+	labels := slice(ids, 1, 1, ids.Shape()[1])        // (B, C-1)
+
+	// TODO: gather
+	_ = props
+	_ = labels
+
+	return nil
+}
+
+func slice(x *variable.Variable, axis, start, end int) *variable.Variable {
+	indices := make([]int, end-start)
+	for i := range indices {
+		indices[i] = start + i
+	}
+
+	return variable.GetItem(axis, indices)(x)
+}
