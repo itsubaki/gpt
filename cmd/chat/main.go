6f2dc3ca5967f974d529b395215fdfd74324fd22
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-08-29 10:39:03 +0900

    Update some files

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -30,7 +30,7 @@ func main() {
 	m.Eval()
 
 	// tokenizer
-	tknizer, err := tokenizer.NewBPETokenizerFrom(mergeRulesPath)
+	bpeTokenizer, err := tokenizer.NewBPETokenizerFrom(mergeRulesPath)
 	if err != nil {
 		panic(err)
 	}
@@ -53,7 +53,7 @@ func main() {
 		tokens := model.GenerateTokens(
 			m,
 			m.MaxContextLen,
-			tknizer,
+			bpeTokenizer,
 			dataloader.AlpacaFormat(prompt),
 			maxNewTokens,
 			temperature,
@@ -69,7 +69,7 @@ func main() {
 		fmt.Println("------------------------------")
 		fmt.Println("generation time:", time.Since(now))
 		fmt.Println("------------------------------")
-		fmt.Println(tknizer.Decode(ids))
+		fmt.Println(bpeTokenizer.Decode(ids))
 		fmt.Println("------------------------------")
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/itsubaki/gpt
 
 go 1.26.1
 
-require github.com/itsubaki/autograd v0.0.8-0.20260814094002-7a0fcef31f96
+require github.com/itsubaki/autograd v0.0.8-0.20260829013430-38983170faa5
